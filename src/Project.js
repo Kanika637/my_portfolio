6c0ad912d027fcc413b5a1a3537f9cd3154dacc7
kanika637 <kanikagola26@gmail.com>
2022-01-11 02:25:50 +0530

    Project pg in progress

--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./styles/project.css"
+import  amazon from "./images/amazon.jpg"
 
 const Project=()=>{
     return(
@@ -8,15 +9,19 @@ const Project=()=>{
             <div className="outer-div">
                 <div className="inner-div">
                     <div className="front">
-                        <img></img>
+                        <img className="ama_img" src={amazon}></img>
                         <div className="front_text">
-                            <he className="front_text-header">Amazon CLone</he>
+                            <h3 className="front_text-header">Amazon Clone</h3>
+                            <h4 className="small">(Using React)</h4>
+
                             <span className="front_text-hover">Hover For Details</span>
                         </div>
                     </div>
                     <div className="back">
+                        <div className="wrapper">
                         <div className="link">
-                            <a href=""></a>
+                            <a target="blank" href="https://en.wikipedia.org/wiki/Taj_Mahal" className="linker"> Project Link</a>
+                            </div>
                         </div>
                     </div>
                 </div>
