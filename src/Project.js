488c131226bf4ffe1422aa259f18415a4a54e3e4
kanika637 <kanikagola26@gmail.com>
2022-01-11 18:13:10 +0530

    Project page started

--- src/Project.js
+++ src/Project.js
@@ -6,6 +6,7 @@ const Project=()=>{
     return(
         <div className="project">
             <div className="background"></div>
+            <div className="first_row">
             <div className="outer-div">
                 <div className="inner-div">
                     <div className="front">
@@ -26,6 +27,34 @@ const Project=()=>{
                     </div>
                 </div>
             </div>
+
+
+
+            <div className="outer-div-2">
+                <div className="inner-div-2">
+                    <div className="front-2">
+                        <img className="ama_img" src={amazon}></img>
+                        <div className="front_text-2">
+                            <h3 className="front_text-header-2">Amazon Clone</h3>
+                            <h4 className="small-2">(Using React)</h4>
+
+                            <span className="front_text-hover-2">Hover For Details</span>
+                        </div>
+                    </div>
+                    <div className="back-2">
+                        <div className="wrapper-2">
+                        <div className="link-2">
+                            <a target="blank" href="https://en.wikipedia.org/wiki/Taj_Mahal" className="linker-2"> Project Link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+
+
+
         </div>
 
     )
